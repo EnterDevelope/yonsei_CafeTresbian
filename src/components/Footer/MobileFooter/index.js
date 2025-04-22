3d3f6aef6d1440bbb6236e1472ca70aff2edfc1d
EnterDevelope <socc2369@naver.com>
2025-04-23 02:04:38 +0900

    style: Footer 모바일 버전 스타일 조정

--- src/components/Footer/MobileFooter/index.js
+++ src/components/Footer/MobileFooter/index.js
@@ -6,34 +6,13 @@ const MobileFooter = () => {
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.logoSection}>
-          <img 
-            src="/images/logo.png" 
-            alt="카페 트레비앙 로고" 
-            className={styles.logo}
-          />
-          <p className={styles.copyright}>
-            © 2024 카페 트레비앙. All rights reserved.
-          </p>
+          <img src="/images/company_logo.png" alt="연세대학교 생활협동조합" className={styles.logo} />
         </div>
-        
         <div className={styles.infoSection}>
-          <div className={styles.contactInfo}>
-            <h3>Contact Us</h3>
-            <p>Email: tresbian@yonsei.ac.kr</p>
-            <p>Phone: 02-2123-4567</p>
-          </div>
-          
-          <div className={styles.locationInfo}>
-            <h3>Location</h3>
-            <p>서울특별시 서대문구 연세로 50</p>
-            <p>연세대학교 학생회관 1층</p>
-          </div>
-          
-          <div className={styles.hoursInfo}>
-            <h3>Hours</h3>
-            <p>월-금: 9:00 - 20:00</p>
-            <p>토-일: 10:00 - 18:00</p>
-          </div>
+          <p>상호명: 연세대학교 생활협동조합 | 이사장: 김용호 | 개인정보관리책임자 : 권훈</p>
+          <p>사업자번호 : 890-82-00336 | 통신판매업신고 : 2021-서울서대문-0417 [사업자정보확인] | TEL : 02-2123-4022 | FAX : 02-312-0471</p>
+          <p>사업장주소 : 서울특별시 서대문구 연세로 50 (신촌동 연세대학교) 학생회관 1층 연세대학교 생활협동조합 (주)037722</p>
+          <p>Email : yonseicoop@yonsei.ac.kr</p>
         </div>
       </div>
     </footer>
